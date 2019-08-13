9fd817e07538c273df9330dd107f0d3d22ad4945
Tyler Koepke <t.koepke10190@gmail.com>
2019-08-13 12:07:42 -0500

    added my CustomList class and my Test class and created a couple tests for the Add method.

---
+++ CustomListTests/AddMethodTests.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using customList;
+
+namespace CustomListTests
+{
+    [TestClass]
+    public class AddMethodTests
+    {
+        [TestMethod]
+        public void Add_AddItemToEmptyList_ItemAddedToIndexZero()
+        {
+            //Arrange
+            CustomList<int> ListOfNumbers = new CustomList<int>();
+            int expected = 1;
+            int actual;
+
+            //Act
+            ListOfNumbers.Add(1);
+            actual = ListOfNumbers[0];
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void Add_AddItemToListWithItem_ItemAddedToEndOfList()
+        {
+            //Arrange
+            CustomList<int> ListOfNumbers = new CustomList<int>();
+            int expected = 2;
+            int actual;
+
+            //Act
+            ListOfNumbers.Add(1);
+            ListOfNumbers.Add(2);
+            actual = ListOfNumbers[1];
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        [TestMethod]
+        public void Add_AddItemToEmptyList_CountIncreasesByOne()
+        {
+            //Arrange
+            CustomList<int> ListOfNumbers = new CustomList<int>();
+            int expected = 1;
+            int actual;
+
+            //Act
+            ListOfNumbers.Add(1);
+            actual = ListOfNumbers.Count;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+
+        }
+
+        public void Add_AddExtraIndiciesToArray_ExtraIndicesAreAddedToEndOfArray()
+        {
+            CustomList<T> 
+        }
+    }
+}
