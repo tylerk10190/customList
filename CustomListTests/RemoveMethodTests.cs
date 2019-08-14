16ff4595ade81fc16ef009417e7b01e286f4a832
Tyler Koepke <t.koepke10190@gmail.com>
2019-08-14 11:59:58 -0500

    Completed my RemoveMethodTests. Added a empty Remove method in CustomList Class.

---
+++ CustomListTests/RemoveMethodTests.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using customList;
+
+namespace CustomListTests
+{
+    [TestClass]
+    public class RemoveMethodTests
+    {
+        
+        [TestMethod]
+        public void Remove_RemoveItemFromListOfOne_ListCountZero()
+        {
+            //Arrange
+            CustomList<int> List = new CustomList<int>();
+            int expected = 0;
+            int actual;
+
+            //Act
+            List.Add(1);
+            List.Remove(1);
+            actual = List.Count;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+       
+       public void Remove_RemoveFirstItemInstance_OnlyOneItemInstanceLeftInList()
+        {
+            //Arrange
+            CustomList<int> List = new CustomList<int>();
+            int expected = 1;
+            int actual;
+
+            //Act
+            List.Add(1);
+            List.Add(1);
+            List.Remove(1);
+            actual = List[1];
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        public void Remove_RemoveFirstItemInstanceInListOfOtherObjects_OnlyOneItemInstanceLeftInList()
+        {
+            //Arrange
+            CustomList<int> List = new CustomList<int>();
+            int expected = 1;
+            int actual;
+
+            //Act
+            List.Add(1);
+            List.Add(2);
+            List.Add(1);
+            List.Remove(1);
+            actual = List[2];
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        public void Remove_RemoveItemFromEmptyList_CountStillZero()
+        {
+            //Arrange
+            CustomList<int> List = new CustomList<int>();
+            int expected = 0;
+            int actual;
+
+            //Act
+            List.Remove(1);
+            actual = List.Count;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+        public void Remove_RemoveItemFromListOfMany_ItemsBehindRemovedItemSlideUpInArray()
+        {
+            //Arrange
+            CustomList<int> List = new CustomList<int>();
+            int expected = 2;
+            int actual;
+
+            //Act
+            List.Add(1);
+            List.Add(2);
+            List.Add(1);
+            List.Remove(1);
+            actual = List[0];
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ RemoveMethodTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RemoveMethodTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RemoveMethodTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("64048d0f-f059-4622-931b-8e29dccb77e4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
