490f5f78e8b92b8aa61d875c4175e4972ad3ccd0
Tyler Koepke <t.koepke10190@gmail.com>
2019-08-18 22:37:22 -0500

    Created one test for the Sort Method and then attempted to create logic for the Sort Method to pass that test. It does not pass the test, but the following the path of the logic through a breakpoint shows that the logic is close. It does move the lowest int to the forefront of the array, but it doesn't add anything else to the list.

---
+++ CustomListTests/SortMethodTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using customList;
+
+namespace CustomListTests
+{
+    [TestClass]
+    public class SortMethodTests
+    {
+        [TestMethod]
+        public void Sort_SortListOfTwoIntsToListOutOfOrder_IntAtIndexOneIsTwo()
+        {
+            CustomList<int> testList = new CustomList<int>();
+            CustomList<int> sortedList = new CustomList<int>();
+            int expected = 2;
+            int actual;
+
+            testList.Add(2);
+            testList.Add(1);
+            sortedList = sortedList.Sort(testList);
+            actual = sortedList[1];
+
+            Assert.AreEqual(expected, actual);
+
+        }
+    }
+}
